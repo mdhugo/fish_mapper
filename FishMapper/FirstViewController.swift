17ce0bb8a5dcdb956f597ddcf059354df7386dc9
quaternarysloth <matt.hugo@gmail.com>
2016-03-31 13:30:12 -0500

    Initial Commit

---
+++ FishMapper/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FishMapper
+//
+//  Created by Matt Hugo on 3/31/16.
+//  Copyright © 2016 hugocorp. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
